8cd3f5274a67a3cc40f0f3deab8f101ef579cbd9
jade ! <150134796+kiitaristi@users.noreply.github.com>
2025-10-15 00:51:29 +0000

    Button created with emoji (step 1 completed)
    Co-authored-by: Cameron Coleman <TheSkyIsForward2@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+
+ <div style="
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  ">
+
+<style>
+.button {
+  border: none;
+  color: white;
+  text-align: center;
+  font-size: 64px;
+}
+
+.button1 {background-color: #ffffff;}
+</style>
+<button class="button button1">🎂</button>
 `;
+
