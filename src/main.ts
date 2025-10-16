f7ca242e90529675bb67111a6f7d5bf024da4a6e
jade ! <150134796+kiitaristi@users.noreply.github.com>
2025-10-16 00:58:34 +0000

    Button increments counter (step 2 completed)
    Co-authored-by: Cameron Coleman <TheSkyIsForward2@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,64 @@
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
-
- <div style="
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+<div style="
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+">
+  <div style="
     margin: 0;
-  ">
+    padding: 0;
+    ">
+    <h1 style="margin: 10;">CMPM 121 Incremental Game</h1>
+    <p style="margin: 4px 20;">Counter: <span id="counter">0</span></p>
+  </div>
 
-<style>
-.button {
-  border: none;
-  color: white;
-  text-align: center;
-  font-size: 64px;
-}
-
-.button1 {background-color: #ffffff;}
-</style>
-<button class="button button1">🎂</button>
+  <button id="button" style="
+    border: none;
+    color: white;
+    text-align: center;
+    font-size: 64px;
+    background-color: #ffffff;
+    cursor: pointer;
+  ">🎂</button>
+</div>
 `;
 
+// Add click handler
+
+const button = document.getElementById("button")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = counter.toString();
+});
+
+// const interval = setInterval(autoIncrease, 1000);
+
+// function autoIncrease(a, b) {
+//   
+//   
+// }
+
+
+//<p style="margin: 4px 0;">Auto Increase: <span id="autoIncrease">0.0</span></p>
+
+// let lastTime = performance.now();
+
+// function update(currentTime: number): void {
+//   const deltaTime = (currentTime - lastTime) / 1000;
+//   lastTime = currentTime;
+
+//   counter += autoInc * deltaTime;
+//   counterDisplay.innerHTML = counter.toFixed(4);
+
+//   requestAnimationFrame(update);
+// }
+
+// requestAnimationFrame(update);
